45f546c0b99d2aadcc5c049adefaa56097bb6a12
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-28 12:33:43 +0300

    add 3 print methods

--- main.rb
+++ main.rb
@@ -1,1 +1,5 @@
-puts "Jesus did it for me again"
+puts "Jesus did it for me again" # prints with a new line 
+
+print "Jesus always does it for me" # prints without a new line
+
+p "Jesus is the King" # prints plus the quotes
