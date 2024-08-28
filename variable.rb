18876a3f418fd4957a8082a007f89678e5ee876e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-28 12:48:09 +0300

    show variable classes

--- variable.rb
+++ variable.rb
@@ -1,10 +1,13 @@
 # string
 hello = "Hello World!"
+puts hello.class
 
 # number
 money = 1000
 price = 100.25
+puts price.class
 
 # booleans
 true_val  = true
 false_val = false  
+puts true_val.class
