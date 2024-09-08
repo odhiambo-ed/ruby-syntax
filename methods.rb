e811e71bb73eaa6c8f5fecf75c4d97b6c118edf3
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 18:55:50 +0300

    add sum method

---
+++ methods.rb
@@ -0,0 +1,15 @@
+def say_hello
+  puts "Hello Jesus! In You I trust"
+end
+
+say_hello  # This will output
+
+# Method Arguments and Return Values
+# 
+def add(a, b)
+  return a + b
+end
+
+sum = add(3, 4)
+puts sum  # This will output 7
+
