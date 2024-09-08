eb20bd1b79a13471c9da4d1ab719ed743dbd3b15
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 17:46:32 +0300

    add each loop

--- loops.rb
+++ loops.rb
@@ -11,7 +11,21 @@ end
 # Until
 # The until loop is the opposite of while. It continues to execute as long as the condition is false.
 
-until i < 5
-  puts "You are at index: #{i}"
-  i += 1
+# until i < 5
+#   puts "You are at index: #{i}"
+#   i += 1
+# end 
+
+# For
+# The for loop iterates over a range or an array.
+
+for item in 1..6
+  puts "Item is #{item}"
+end
+
+# Each
+# The each method is commonly used to iterate over arrays and hashes.
+
+[1, 2, 3, 4, 5, 6, 7].each do |s|
+  puts "The number is #{s}"
 end
