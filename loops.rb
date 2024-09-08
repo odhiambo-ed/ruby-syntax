742d7cc855757951fedbf10b329e01f3443e5c33
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 17:41:06 +0300

    add until loop

--- loops.rb
+++ loops.rb
@@ -1,8 +1,17 @@
 # While loop
+# The while loop continues to execute as long as the condition is true.
 
 i = 0
 
 while i < 10
   puts "You are at position: #{i}"
+  i += 1
+end
+
+# Until
+# The until loop is the opposite of while. It continues to execute as long as the condition is false.
+
+until i < 5
+  puts "You are at index: #{i}"
   i += 1
 end
