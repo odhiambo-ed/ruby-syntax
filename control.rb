5f5ea4c4c102aa4f15a349eaf10ab72eccaf1112
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 17:27:04 +0300

    add if condition

---
+++ control.rb
@@ -0,0 +1,11 @@
+number = -20
+
+if number > 10
+  puts "Number is greater than 10"
+elsif number == 10
+  puts "Number is equal to 10"
+elsif number < 10
+  puts "Number is less than 10"
+else
+  puts "Invalid input"
+end
