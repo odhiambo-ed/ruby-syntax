396416635f7a1d6e41f03190bbfdf14f7ed035ca
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 19:20:17 +0300

    add a class

---
+++ classes.rb
@@ -0,0 +1,19 @@
+# Learn the basics of OOP in Ruby.
+# 
+# Topics:
+# 
+# Classes and Objects
+# Instance Variables and Methods
+# Class Variables and Methods
+# Inheritance
+# 
+# In Ruby, everything is an object. A class is a blueprint for creating objects (instances)
+# 
+
+
+# Define a class
+class Person
+end
+
+# Create an object (instance) of the class
+person = Person.new
