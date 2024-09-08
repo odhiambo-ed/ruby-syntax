b29bea110f1148f6320117e329a5475cf35916e0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 19:01:10 +0300

    add methods with blocks

--- methods.rb
+++ methods.rb
@@ -13,3 +13,15 @@ end
 sum = add(3, 4)
 puts sum  # This will output 7
 
+
+# Blocks, yield, and Block Parameters
+# Blocks are chunks of code that can be passed to methods. 
+# They are enclosed in either curly braces {} or do...end. 
+# You can call a block within a method using the yield keyword.
+# 
+
+def great
+  yield if block_given?
+end
+
+great {puts "Hello Jesus my Lord"}
