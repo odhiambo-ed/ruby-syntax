1590bd9a4fbca4a1c6c1fb938646153dabcd80d1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-08 17:53:04 +0300

    add exercise one

---
+++ exercise-1.rb
@@ -0,0 +1,10 @@
+# Exercise 1: Check if a Number is Even or Odd
+# Write a Ruby script that checks if a number is even or odd.end
+# 
+number = 12349
+
+if number.even?
+  puts "Number is Even"
+else
+  puts "Number is odd"
+end
